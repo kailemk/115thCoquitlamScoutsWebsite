945ad3afde4e9c670ddb107a4c315188c1b0aa61
DrPepperStud <kailemhillm@gmail.com>
2020-08-29 16:08:57 -0700

    Posting for review

---
+++ javascript/paragraphs.js
@@ -0,0 +1,21 @@
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+window.onload = function init() {
+    readTextFile("../announcements/christmas-camp-2019.txt")
+}

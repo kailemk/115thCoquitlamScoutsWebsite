b265ac8bfca5444aa3058c1464a0ba5913f842ab
DrPepperStud <kailemhillm@gmail.com>
2021-01-08 18:32:53 -0800

    head - refactored file locations

--- javascript/common-html.js
+++ javascript/common-html.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
         $("#header-desktop").load("html/header/header-desktop.html"); 
         $("#footer-desktop").load("html/footer/footer-desktop.html");
         $("#header-mobile").load("html/header/header-mobile.html"); 
-        $("#footer-mobile").load("html/footer/footer-mobile.html"); 
+        $("#footer-mobile").load("html/footer/footer-mobile.html");
+        $("head").append(readTextFile("html/favicon/favicon.html"));
 });
---
+++ javascript/helper.js
@@ -0,0 +1,46 @@
+function readTextFile(file){
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    var allText;
+    rawFile.onreadystatechange = function (){
+        if(rawFile.readyState === 4){
+            if(rawFile.status === 200 || rawFile.status == 0){
+                allText = rawFile.responseText;
+            }
+        }
+    }
+    rawFile.send(null);
+    return allText;
+}
+
+function readTextFile(file){
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    var allText;
+    rawFile.onreadystatechange = function (){
+        if(rawFile.readyState === 4){
+            if(rawFile.status === 200 || rawFile.status == 0){
+                allText = rawFile.responseText;
+            }
+        }
+    }
+    rawFile.send(null);
+    return allText;
+}
+
+function parseText(text){
+    var lines = text.split('\n');
+    var html = "";
+    for(var i = 0; i < lines.length; i++){
+        html += lines[i];
+    }
+    return html;
+}
+
+function generateParagraphHTML(file, className){
+    var text = readTextFile(file);
+    text = parseText(text);
+    var elements = document.getElementsByClassName(className);
+    elements[0].insertAdjacentHTML('afterbegin', "<p>" + text + "</p>");
+    elements[1].insertAdjacentHTML('afterbegin', "<p>" + text + "</p>");
+}

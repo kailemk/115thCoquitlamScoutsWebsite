12db61ca90c88e4c202296ed01ebca07e929138f
DrPepperStud <kailemhillm@gmail.com>
2021-01-10 23:57:19 -0800

    announcement - jan stage 2 announcement made

--- javascript/announcements.js
+++ javascript/announcements.js
@@ -3,4 +3,5 @@ document.addEventListener('DOMContentLoaded', function () {
 
 $(document).ready(function() {
     $(".first-meeting-2021").load("./html/announcements/first-meeting-2021.html"); 
+    $(".jan-stage2-2021").load("./html/announcements/jan-stage2-2021.html"); 
 });

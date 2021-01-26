42a67c9a8fdc0f9d480a844169630fc45b2ae03a
kailemk <kailem@mlvx.io>
2021-01-26 00:56:22 -0800

    announcements : no meeting announcement

--- javascript/announcements.js
+++ javascript/announcements.js
@@ -4,5 +4,6 @@ document.addEventListener('DOMContentLoaded', function () {
 $(document).ready(function() {
     $(".first-meeting-2021").load("./html/announcements/first-meeting-2021.html"); 
     $(".jan-stage2-2021").load("./html/announcements/jan-stage2-2021.html"); 
-    $(".first-meeting-01-2021").load("./html/announcements/01-2021/first-meeting.html"); 
+    $(".first-meeting-01-2021").load("./html/announcements/01-2021/first-meeting.html");
+    $(".no-meeting-01-2021").load("./html/announcements/01-2021/no-meeting.html");
 });

bc2f9ea83615b049f4fbd853cce127d16a332bcb
kailemk <kailem@mlvx.io>
2021-02-01 23:38:18 -0800

    announcement: feb meeting 1

--- javascript/announcements.js
+++ javascript/announcements.js
@@ -6,4 +6,5 @@ $(document).ready(function() {
     $(".jan-stage2-2021").load("./html/announcements/jan-stage2-2021.html"); 
     $(".first-meeting-01-2021").load("./html/announcements/01-2021/first-meeting.html");
     $(".no-meeting-01-2021").load("./html/announcements/01-2021/no-meeting.html");
+    $(".firestarting-02-2021").load("./html/announcements/02-2021/firestarting.html");
 });

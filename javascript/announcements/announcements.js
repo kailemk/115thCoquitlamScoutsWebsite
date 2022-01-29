ccbfe69b0ed54df0f6c83ae82439cb263d5ef640
kailemk <kailemhillm@gmail.com>
2022-01-28 22:29:59 -0800

    january 2022 update

--- javascript/announcements/announcements.js
+++ javascript/announcements/announcements.js
@@ -6,6 +6,17 @@ $(document).ready(function() {
 
     new AnnouncementsGenerator([
         new Announcement({
+            title: "Meeting Plan for Feb 4", 
+            date: "January 31, 2022", 
+            time: "10:00:00",
+            contentPath: "./html/announcements/01-2022/update9.html",
+            image: "images/hatchet.png", 
+            imgAlt: "Hatchet", 
+            imgHeight: "200px",
+            forceMobile: false,
+            buttons: []
+        }),
+        new Announcement({
             title: "No meeting on October 8 Reminder", 
             date: "October 4, 2021", 
             time: "10:00:00",
@@ -104,7 +115,8 @@ $(document).ready(function() {
             time: "08:00:00",
             contentPath: "./html/announcements/09-2021/update3.html",
             image: "images/hose.png", 
-            imgAlt: "Hose", 
+            imgAlt: "Hose",image: "images/hatchet.png", 
+            imgAlt: "Hatchet", 
             imgHeight: "200px",
             forceMobile: false,
             buttons: []
@@ -175,12 +187,8 @@ $(document).ready(function() {
             forceMobile: true,
             buttons: [
                 new Button({
-                title: "Camp Form",
-                link: "https://forms.gle/7Z4NqR5yukY1XHzS8"
-            })]
-        }),
-        new Announcement({
-            title: "Meeting this Week (Aug. 6th)", 
+                title: "Camimage: "images/hatchet.png", 
+                imgAlt: "Hatchet", this Week (Aug. 6th)", 
             date: "August 4, 2021", 
             time: "09:00:00",
             contentPath: "./html/announcements/08-2021/update1.html",
@@ -269,10 +277,8 @@ $(document).ready(function() {
                 link: "https://forms.gle/3KccGhXmfVA9XvUT8"
             })]
         }),
-        new Announcement({
-            title: "Survey and Summer Meetings Reminder", 
-            date: "July 1, 2021", 
-            time: "08:00:00",
+        new Announcement({image: "images/hatchet.png", 
+        imgAlt: "Hatchet",",
             contentPath: "./html/announcements/07-2021/update1.html",
             image: "images/survey.png", 
             imgAlt: "Survey Form", 
@@ -368,8 +374,8 @@ $(document).ready(function() {
                 title: "Sign Up",
                 link: "https://docs.google.com/forms/d/e/1FAIpQLScz4Vq7Y-To9lLTkqMhdbsXDQnzQQm4Tme2aVYqA0RC0TnSeg/viewform"
             })
-            ]
-        }),
+            ]image: "images/hatchet.png", 
+            imgAlt: "Hatchet",
         new Announcement({
             title: "May 30th Hike", 
             date: "May 11, 2021", 
